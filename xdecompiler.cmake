17b29ba347fd954ff83d8eddc6d5b4b69444cc54
horsicq <horsicq@gmail.com>
2024-12-17 00:49:55 +0100

    Update file(s): 2024-12-17

--- xdecompiler.cmake
+++ xdecompiler.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XDECOMPILER_SOURCES
+    ${XDECOMPILER_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xdecompiler.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xdecompiler.h
 )
